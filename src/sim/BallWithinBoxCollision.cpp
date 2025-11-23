d9c1d8b7178d4ddb221fd1814833704ecb3e0634
Trevor Pham <trevor.dt.pham@gmail.com>
2025-11-22 20:43:47 -0500

    refactor magic numbers

--- src/sim/BallWithinBoxCollision.cpp
+++ src/sim/BallWithinBoxCollision.cpp
@@ -10,17 +10,18 @@ class BallWithinBoxCollision : public sim::Simulation {
 private:
 	sf::CircleShape shape_;
 	sf::Vector2f velocity_;
+	const float radius_ = 12.f;
 	sf::Vector2u boxSize_{ 800, 600 };
 public:
 	BallWithinBoxCollision() {
-		velocity_ = sf::Vector2f(140.f, 100.f);
+		velocity_ = sf::Vector2f(230.f, 150.f);
 	}
 
 	std::string name() const override { return "Ball Within Box Collision"; }
 
 	void init(sf::RenderWindow& window) override {
 		boxSize_ = window.getSize();
-		shape_.setRadius(12.f);
+		shape_.setRadius(radius_);
 		shape_.setFillColor(sf::Color::Blue);
 		shape_.setPosition(sf::Vector2(
 			static_cast<float>(boxSize_.x) / 2.f,
@@ -35,13 +36,25 @@ public:
 
 		// collision with a wall causes the velocity component that caused the impact to be inverted
 		// collision with left-hand boundary
-		if (pos.x < 0.f) { pos.x = 0.f; velocity_.x = std::abs(velocity_.x); }
+		if (pos.x < 0.f) {
+			pos.x = 0.f;
+			velocity_.x = std::abs(velocity_.x);
+		}
 		// collision with top boundary
-		if (pos.y < 0.f) { pos.y = 0.f; velocity_.y = std::abs(velocity_.y); }
+		if (pos.y < 0.f) {
+			pos.y = 0.f;
+			velocity_.y = std::abs(velocity_.y);
+		}
 		// collision with right-hand boundary
-		if (pos.x > static_cast<float>(boxSize_.x)) { pos.x = static_cast<float>(boxSize_.x); velocity_.x = -std::abs(velocity_.x); }
+		if (pos.x > static_cast<float>(boxSize_.x) - 2*radius_) {
+			pos.x = static_cast<float>(boxSize_.x) - 2*radius_;
+			velocity_.x = -std::abs(velocity_.x);
+		}
 		// collision with bottom boundary
-		if (pos.y > static_cast<float>(boxSize_.y)) { pos.y = static_cast<float>(boxSize_.y); velocity_.y = -std::abs(velocity_.y); }
+		if (pos.y > static_cast<float>(boxSize_.y) - 2*radius_) {
+			pos.y = static_cast<float>(boxSize_.y) - 2*radius_;
+			velocity_.y = -std::abs(velocity_.y);
+		}
 
 		shape_.setPosition(pos);
 	}
